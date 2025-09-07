3140f2595441cf36e8706c85328e02407183d3b3
hanqian <2590332461@qq.com>
2025-09-07 09:47:40 +0800

    reInit: 项目重新初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"), // 设置 `@` 指向 `src` 目录
+    },
+  },
+  server: {
+    port: 4000,
+    cors: true,
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
